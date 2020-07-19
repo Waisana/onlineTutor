faae8043f4497c75f3f14c2e41f8e2e6b294e897
Austinove <bryanovicaustenove@gmail.com>
2020-07-19 14:27:37 +0300

    added forms in prifile screen

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -40,6 +40,42 @@
                         <h4>
                             <strong>Make changes</strong>
                         </h4>
+                        <div class="row mt-4">
+                            <div class="col-md-6">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">First Name</label>
+                                        <input type="text" class="form-control" placeholder="first name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Second Name</label>
+                                        <input type="text" class="form-control" placeholder="second name">
+                                    </div>
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </form>
+                            </div>
+                            <div class="col-md-6">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Email address</label>
+                                        <input type="email" class="form-control" placeholder="User Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Password</label>
+                                        <input type="password" class="form-control" placeholder="Password">
+                                    </div>
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
