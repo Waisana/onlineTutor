6b7eee49c0d806cb3b2033557663993ab4f0532b
Waisana Bosco <37265882+Waisana@users.noreply.github.com>
2020-07-21 00:03:57 +0300

    login page some what sorted check it out

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('auth.authApp')
 @section('welcome')
-<div class="container">
+<div class="container main-face">
     <div class="row ">
         <div class="col-md-8">
             <div class="title-intro">
@@ -20,20 +20,54 @@
                         <h3 class="mb-2 font-weight=bold"> Sign in</h3>
                         <input type="text" class="input-block-level form-control input-lg mb-2" name="username" placeholder="Username">
                         <input type="password" class="input-block-level form-control input-lg mb-2"  name="password" placeholder="Password" >
-                        <button name="login" class="btn btn-warning btn-md " type="submit"> Sign in</button> 
+                        <button name="login" class="btn btn-md " type="submit"> Sign in</button> 
                     </div>
                 </form>	
             </div>
             <div class="form-singUp">
                 New to Godka E-learning system
-                <hr>
+                <hr class="bg-white">
                 <h3 class="mb-2 font-weight=bold">Sign up</h3>
                 <div class="signup-buttons">
-                    <a href="{{route('studentsRegister')}}"  name="login" class="btn btn-warning btn-md " type="submit">I`m a Student</a>
-                    <a href="{{route('teachersRegister')}}"   name="login" class="btn btn-warning btn-md " type="submit">I`m a Teacher</a>
+                    <a href="{{route('studentsRegister')}}"  name="login" class="btn  btn-md " type="submit">I`m a Student</a>
+                    <a href="{{route('teachersRegister')}}"   name="login" class="btn btn-md " type="submit">I`m a Teacher</a>
                 </div>
             </div>
         </div>
+
+        <footer >
+            <div class="container">
+                <div class="row">
+    
+                    <div class="col-md-6 ">
+                        <div class="card bg-transparent"> 
+                            <div class="card-body">
+                                <p class="simplenav">
+                                    <a href="login.php">I`m a Student</a> | 
+                                    <a href="login.php">I`m a Teacher</a> |
+                                    <a href="contact.php">www.godkatechnologies.com</a>
+                                </p>
+                            </div>
+                        </div>  
+                    </div>
+    
+                    <div class="col-md-6">
+                        <div class="card bg-transparent">
+                            <div class="card-body">
+                                <p class="text-right">
+                                    &copy; 2020 onlinetutor  
+                                </p>
+                            </div>
+                        </div> 
+                    </div>
+    
+                </div>
+                <!-- /row of panels -->
+            </div>
+        
+        </footer>
+
     </div>
-</div>
+
+   
 @endsection
